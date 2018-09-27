458eb2d3c6bfa8859c68620899d4ab1dabc788c5
kevy <kv_vinu@hotmail.com>
2018-09-27 15:55:05 +0530

    changed name and more

---
+++ assets/js/type.js
@@ -0,0 +1,8 @@
+$(function(){
+ $(".typed").typed({
+  strings: ["Linux.","open-source.","daily needs of Linux.","AWS.","Google Cloud.","OpenStack.","virtualization.","networking.","Oracle Database Appliance.","automation scripts."],
+	typeSpeed: 100,
+	loop: true,
+	backDelay: 1000
+	});
+});
